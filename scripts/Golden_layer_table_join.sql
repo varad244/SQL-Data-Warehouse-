2df631bd84beb2f31b64a38ad94527d67456874a
Varad Digraskar <143405252+varad244@users.noreply.github.com>
2025-12-08 23:45:51 +0530

    Create Golden_layer_table_join.sql

---
+++ scripts/Golden_layer_table_join.sql
@@ -0,0 +1,16 @@
+SELECT 
+    ci.cst_id,
+    ci.cst_key,
+    ci.cst_firstname,
+    ci.cst_lastname,
+    ci.cst_marital_status,
+    ci.cst_gndr,
+    ci.cst_create_date,
+    ca.bdate,
+    ca.gen,
+    la.cntry
+FROM silver.crm_cust_info AS ci
+left join silver.erp_cust_az12 as ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 as la
+on ci.cst_key = ca.cid
