e88c074377a427fd3c3a8462d2db262b8b4d961c
Varad Digraskar <143405252+varad244@users.noreply.github.com>
2025-12-09 11:45:12 +0530

    Update Golden_layer_table_join.sql

--- scripts/Golden_layer_table_join.sql
+++ scripts/Golden_layer_table_join.sql
@@ -1,14 +1,17 @@
+create view gold.dim_customer as
 SELECT 
-    ci.cst_id,
-    ci.cst_key,
-    ci.cst_firstname,
-    ci.cst_lastname,
-    ci.cst_marital_status,
-    ci.cst_gndr,
-    ci.cst_create_date,
-    ca.bdate,
-    ca.gen,
-    la.cntry
+	row_number() over(order by cst_id) as customer_key,
+    ci.cst_id as Customer_id,
+    ci.cst_key as Customer_no,
+    ci.cst_firstname as firstname,
+    ci.cst_lastname as lastname,
+    ci.cst_marital_status as marital_status,
+	Case when ci.cst_gndr != 'n/a' then ci.cst_gndr 
+		 else coalesce(ca.gen,'n/a')
+	end as gender,
+    ci.cst_create_date as create_date,
+    ca.bdate as birthdate,
+    la.cntry as country
 FROM silver.crm_cust_info AS ci
 left join silver.erp_cust_az12 as ca
 on ci.cst_key = ca.cid
